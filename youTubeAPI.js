8f60184bb514345a37419b28be4fb9717d53858d
Mubarek Mussa <mubarekmussa@Mubareks-MBP.fios-router.home>
2018-09-11 21:46:02 -0400

    first youTube API commit

---
+++ youTubeAPI.js
@@ -0,0 +1,54 @@
+const youTube_URL = "https://www.googleapis.com/youtube/v3/search";
+
+function get_YouTube_API(curr, callback){
+  const settings ={
+    url: youTube_URL,
+    id:{
+      kind:'youtube#video'
+    },
+    data : {
+      part:'snippet',
+      key:'AIzaSyA_btGl7SDfpHrSXexVkeSNiXW5111kg0w',
+      q: `${curr}`
+    },
+    dataType: 'json',
+    type: 'GET',
+    maxResults:5,
+    success: callback
+
+  }
+
+  $.ajax(settings);
+}
+
+function renderResult(items){
+
+  if(items.id.kind === 'youtube#video'){
+  console.log(items.snippet.thumbnails.medium.url);
+  console.log(items.id.videoId);
+  console.log(items);
+  
+  return `<div>
+      <a href = "https://www.youtube.com/watch?v=${items.id.videoId}"><img src = "${items.snippet.thumbnails.medium.url}" alt = "Thumbnail of pic"></a>
+    </div>`; 
+  //this returns the urls to the possible thumbnails that we use
+  //now we need to get the link to the video 
+  }
+} 
+
+
+function displayYouTubeSearchData(data) {
+  //.map helps iterate through every element.
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').append(results);
+}
+
+function get_Input(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    let curr = $('.insertInfo').val();
+    get_YouTube_API(curr, displayYouTubeSearchData);
+  });
+}
+
+$(get_Input);
