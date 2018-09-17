74a77f3b93b2c0b0cda58f21e9ee2030a88ade4f
Mubarek Mussa <mubarekmussa@Mubareks-MacBook-Pro.local>
2018-09-17 19:40:07 -0400

    Thinktube a11y refactor pt1

--- youTubeAPI.js
+++ youTubeAPI.js
@@ -1,6 +1,12 @@
 const youTube_URL = "https://www.googleapis.com/youtube/v3/search";
+let numOfResults = 0;
+let checkBoolean = false;
 
 function get_YouTube_API(curr, callback){
+  if(checkBoolean == true){
+    numOfResults = 0;
+  }
+
   const settings ={
     url: youTube_URL,
     id:{
@@ -13,29 +19,39 @@ function get_YouTube_API(curr, callback){
     },
     dataType: 'json',
     type: 'GET',
-    maxResults:5,
+    maxResults:10,
     success: callback
-
   }
 
   $.ajax(settings);
 }
 
 function renderResult(items){
-
   if(items.id.kind === 'youtube#video'){
   console.log(items);
+  numOfResults++;
   return `<div>
       <a href = "https://www.youtube.com/watch?v=${items.id.videoId}"><img src = "${items.snippet.thumbnails.medium.url}" alt = "Thumbnail of pic"></a>
     </div>`; 
+    
   }
+  console.log(numOfResults);
 } 
 
+function displayNumberOfResults(){
+  console.log("in here");
+  let results = $('.numberOfResults');
+  results.html(`<p>Number of results:  ${numOfResults}</p>`);
+  console.log(numOfResults);
+}
 
 function displayYouTubeSearchData(data) {
   //.map helps iterate through every element.
   const results = data.items.map((item, index) => renderResult(item));
-  $('.js-search-results').append(results);
+  $('.js-search-results').prop('hidden', false);
+  $('.js-search-results').html(results);
+  displayNumberOfResults();
+  checkBoolean = true;
 }
 
 function get_Input(){
